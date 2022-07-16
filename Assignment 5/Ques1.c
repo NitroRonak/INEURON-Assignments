af3fb30cd8347a03e2e86f5bc4f3a2204a6a2b53
NitroRonak <99889967+NitroRonak@users.noreply.github.com>
2022-07-16 15:17:49 +0530

    Add files via upload

---
+++ Assignment 5/Ques1.c
@@ -0,0 +1,12 @@
+// Write a program to print MySirG n times on the screen
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int i=0;
+    while(i<n){
+        printf("MySirG\n");
+        i++;
+    }
+    return 0;
+}

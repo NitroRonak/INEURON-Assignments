a78ebeea26a3f563a20f7cd66a2a0447d54d6792
NitroRonak <99889967+NitroRonak@users.noreply.github.com>
2022-07-30 13:39:44 +0530

    Create Ques1.c

---
+++ Assignment 12/Ques1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void N_naturalNumber(int n){
+    if(n==0)
+        return;
+    N_naturalNumber(n-1);
+    printf("%d ",n);
+}
+int main(){
+    int n;
+    printf("Enter the value of N:\n");
+    scanf("%d",&n);
+    N_naturalNumber(n);
+    return 0;
+}
